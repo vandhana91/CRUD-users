8e4458d6e83d7c1e395795534a017021d16dc534
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2022-04-24 19:11:44 +0530

    Add files via upload

---
+++ components/array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id:'1', 
+        Name:'Shiva',
+        Age:'23'
+    },
+    {
+        id:'2', 
+        Name:'Sree',
+        Age:'22'
+    },
+    {
+        id:'3',
+        Name:'Akshay',
+        Age:'23'
+    }
+]
+  
+export default array
